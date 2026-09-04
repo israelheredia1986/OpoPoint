28206fdd967516ed3f5ccfadc31fe01049f06645
israelheredia1986 <israelheredia1986@gmail.com>
2026-09-04 12:23:51 +0200

    Forzar actualización de caché PWA para nueva interfaz OpoPoint

--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-const CACHE='opopoint-v2-20260904';
-const ASSETS=['./','./index.html','./datos.js','./manifest.webmanifest'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache=>cache.addAll(ASSETS))
-      .then(()=>self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached=>cached||fetch(event.request))
-  );
-});
+const CACHE = 'opopoint-v3-20260904';
+const CORE = ['./','./index.html','./datos.js','./manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => { if(event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(cache => cache.put(event.request,copy)); return response; }).catch(() => caches.match('./index.html'))); });
