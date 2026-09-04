7f0764d4fc2396df76ec1981be3ebf41a993639e
israelheredia1986 <israelheredia1986@gmail.com>
2026-09-04 12:19:28 +0200

    Actualizar caché del PWA tras rediseño OpoPoint

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-const CACHE='opopoint-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./datos.js','./manifest.webmanifest']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='opopoint-v2-20260904';
+const ASSETS=['./','./index.html','./datos.js','./manifest.webmanifest'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request))
+  );
+});
